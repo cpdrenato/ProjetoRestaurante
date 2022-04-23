c0d5fcf759e480f3057b1224be21cbffaaf5b940
jvitorsena <jvitorsena96@gmail.com>
2022-04-23 16:29:45 -0300

    footer

--- index.php
+++ index.php
@@ -1234,7 +1234,7 @@
 
   <!-- Start Map section -->
   <section id="mu-map">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9207.358598888495!2d-85.64847801496286!3d30.183918972289003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0x2320479d70eb6202!2sDillard&#39;s!5e0!3m2!1sbn!2sbd!4v1462359735720" width="100%" height="100%" frameborder="0" allowfullscreen></iframe>
+    <iframe src="https://maps.google.com/maps?q=Avenida%20Governador%20Jos%C3%A9%20Malcher,%20Bel%C3%A9m,%20Brasil&t=&z=15&ie=UTF8&iwloc=&output=embed" width="100%" height="100%" frameborder="0" allowfullscreen></iframe>
   </section>
   <!-- End Map section -->
 
@@ -1244,15 +1244,8 @@
       <div class="row">
         <div class="col-md-12">
           <div class="mu-footer-area">
-            <div class="mu-footer-social">
-              <a href="#"><span class="fa fa-facebook"></span></a>
-              <a href="#"><span class="fa fa-twitter"></span></a>
-              <a href="#"><span class="fa fa-google-plus"></span></a>
-              <a href="#"><span class="fa fa-linkedin"></span></a>
-              <a href="#"><span class="fa fa-youtube"></span></a>
-            </div>
             <div class="mu-footer-copyright">
-              <p>Designed by <a rel="nofollow" href="http://www.markups.io/">MarkUps.io</a></p>
+              <p>Desenvolvido pelos alunos do 7º Periodo da faculdade Doctum de Ipatinga</p>
             </div>
           </div>
         </div>
