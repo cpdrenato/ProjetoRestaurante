5139b388123a8c0907cb2e4d4c9c8846e48b60a3
jvitorsena <jvitorsena96@gmail.com>
2022-04-23 15:48:42 -0300

    commit

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<?php require_once("config.php") ?> <!-- Chamada do arquivo config.php -->
+<?php require_once("config.php") ?>
+<!-- Chamada do arquivo config.php -->
 
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -73,7 +74,7 @@
           <!-- LOGO -->
           <a class="navbar-brand" href="index.html"><img src="assets/img/logo.png" alt="Logo img"></a>
         </div>
-        
+
         <div id="navbar" class="navbar-collapse collapse">
           <ul id="top-menu" class="nav navbar-nav navbar-right mu-main-nav">
             <li><a href="#mu-slider">Início</a></li>
@@ -95,7 +96,7 @@
   <section id="mu-slider">
     <div class="mu-slider-area">
       <div class="mu-top-slider">
-        
+
         <div class="mu-top-slider-single">
           <img src="assets/img/slider/1.jpg" alt="img">
           <div class="mu-top-slider-content">
@@ -168,7 +169,7 @@
                 </div>
               </div>
               <!-- fim slider -->
-              
+
             </div>
           </div>
         </div>
@@ -1255,7 +1256,9 @@
   <!-- End Contact section -->
 
   <!-- Start Map section -->
-
+  <section id="mu-map">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9207.358598888495!2d-85.64847801496286!3d30.183918972289003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0x2320479d70eb6202!2sDillard&#39;s!5e0!3m2!1sbn!2sbd!4v1462359735720" width="100%" height="100%" frameborder="0" allowfullscreen></iframe>
+  </section>
   <!-- End Map section -->
 
   <!-- Start Footer -->
